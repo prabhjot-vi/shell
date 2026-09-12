4605b47b84a7722f373600c522d6fa38e78c82d2
prabhjot-vi <prabhjotsingh.vi@gmail.com>
2026-09-12 13:52:09 +0000

    added quit, help and error function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,43 @@
 #include<iostream>
 
+void print_help() {
+    std::cout << "Commands:\n" 
+            << "\thelp\t\tprints help\n"
+            << "\tquit or ctrl+c\t\tquit out of shell\n";
+}
+
+void print_unknow_command(std::string_view command) {
+    std::cout << "Command \""
+            << command
+            << "\" not found\n"
+            << "Try: help\n";
+}
+
 int main() {
     std::string input;
-    std::cout << "> ";
-    std::getline(std::cin, input);
 
-    std::cout << "Input: " << input << "\n";
+    bool running = true;
+
+    while(running) {
+        std::cout << "> ";
+        std::cin >> input;
+
+        if (input == "") {
+            continue;
+        }
+        else if (input == "quit") {
+            running = false;
+            continue;
+        }
+        else if (input == "help") {
+            print_help();
+            continue;
+        }
+        else {
+            print_unknow_command(input);
+            continue;
+        }
+    }
+
     return 0;
 }
